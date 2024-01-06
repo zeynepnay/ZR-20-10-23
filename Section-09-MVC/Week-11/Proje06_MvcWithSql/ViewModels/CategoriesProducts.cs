16f9e6755e881ffde30b10ecb97a09ba180a41de
zeynepnay <zeynepay043@gmail.com>
2024-01-06 14:51:24 +0300

    6-10-2024

---
+++ Section-09-MVC/Week-11/Proje06_MvcWithSql/ViewModels/CategoriesProducts.cs
@@ -0,0 +1,10 @@
+﻿namespace Proje06_MvcWithSql.ViewModels
+{
+    public class CategoriesProducts
+    {
+        public List<CategoryViewModel> Categories{ get; set; }
+        public List<ProductViewModel> Products { get; set; }
+
+
+    }
+}
